c5e87187e4ad002405128fb5b810df3582de3a24
salanho <espiritosanto.gabriel@gmail.com>
2019-06-11 00:09:57 -0300

    trab-oac processador feito do 0

--- DataPath.v
+++ DataPath.v
@@ -36,7 +36,8 @@ input wire iCLK, iCLKMem, iCLK50, iRST;
 output wire [63:0] wPC, wRegisterShow;
 output wire [31:0] woInstruction;
 output wire wControlRegWrite, wControlMemRead, wControlMemWrite;
-output wire [1:0] wControlReg2Loc, wControlALUOp, wControlALUSrc, wControlOrigemPC, wControlMemtoReg;
+output wire [1:0] wControlALUOp, wControlOrigemPC;
+output wire wControlReg2Loc, wControlALUSrc, wControlMemtoReg;
 output wire [10:0] wControlOpcode;
 input wire [4:0] wRegisterShowSelect;
 //Para Debug
@@ -85,14 +86,78 @@ wire wZero;
 wire [3:0] wALUControl;
 wire [63:0] wReadData;
 wire [63:0] wDataRegister;
-wire [25:0] wImmediate;
-wire [63:0] wImmediateExtension;
-wire [63:0] wBranchPC;
-wire [63:0] wImmediateZeroExtension; //wExtZeroImm;
+//USADO APENAS PARA REPLICAR OS BITS NA EXTENSAO DE BITS DA INSTRUCAO ABAIXO
+wire [31:0] wZero32; 
+//EXTENSAO DE SINAL DE 32 PARA 64 BITS, nao sei se ta funcionando corretamente ainda 
+wire [63:0] wInstructionExtended;
 
+//POSTERIOR IMPLEMENTACAO
+//wire [63:0] wBranchPC;
+/******************************************************************************
+					NAO ACREDITO QUE SERAO IMPLEMENTADOS ESSES MAS SO POR GARANTIA
+					VER DEPOIS
+					//wire [25:0] wImmediate;
+					//wire [63:0] wImmediateExtension;
+					//wire [63:0] wImmediateZeroExtension; //wExtZeroImm;
+*******************************************************************************/
 initial
 begin
-	PC <= 32'b0;
+	PC <= 64'b0;
 end
+
+assign wPC_MAIS_4 = wPC + 64'h4;	// wPC_MAIS_4 = wPC + 4
+//assign wBranchPC;
+assign wPC = PC;	//manda valor do registrador PC para wirePC
+assign wControlOpcode = wInstruction[31:21];
+assign wAddressRm = wInstruction[20:16];
+assign wAddressRn = wInstruction[9:5];
+assign wAddressRt = wInstruction[4:0];
+assign woInstruction = wInstruction;
+assign wZero32 = 32'b0;
+assign wInstructionExtended = {wZero32, wInstruction};
+
+//Memoria de Instrucoes
+
+//Banco de Registradores
+
+//ALU Control
+
+//ALU
+
+//Memoria de Dados
+
+//Control
+
+always @(wControlReg2Loc)
+begin
+	case(wControlReg2Loc)
+		1'b0:
+			wControlReg2Loc <= wAddressRm;
+		1'b1:
+			wControlReg2Loc <= wAddressRt;
+	
+	endcase
+end
+
+always @(wControlALUSrc)
+begin
+	case(wControlALUSrc)
+		1'b0:
+			wControlALUSrc <= wRead2;
+		1'b1:
+			wControlALUSrc <= wInstructionExtended;
+	endcase
+end
+
+always @(wControlOrigemPC)
+begin
+	case(wControlOrigemPC)
+		1'b0:
+			wControlOrigemPC <= wPC_MAIS_4;
+		//PARA SER 1 PRECISA DE UM AND ENTRE BRANCH E ZERO
+		//1'b1:
+	endcase
+end
+
 
 endmodule
---
+++ readme.md
@@ -0,0 +1,6 @@
+#Trabalho OAC 2019-1 Universidade de Brasilia
+## Desenvolvido por:
+- Gabriel Oliveira do Espírito Santo
+- Carlos Gabriel (completa aqui dps nao sei o nome todo hhaahhahaha)
+- Andrey Calaça Resende
+- Divino (nao sei o nome ocmpleto tb, completa aqui hahahahahha)
